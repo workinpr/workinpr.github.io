a4f93a3d3528dca428acb9a8a853269588b4887c
Jose Velez <josevelezq7@gmail.com>
2018-09-05 00:15:49 -0400

    Fixed nav bar a bit, refactored CSS. Still fixing the responsivity of 3 sections under process.

---
+++ scripts/scroll.js
@@ -0,0 +1,9 @@
+        $(document).on('click', 'a[href^="#"]', function (event) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 1000);
+        });
+
+        window.onscroll = function() {stickyOnTop()};
